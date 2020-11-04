b6f9fae75522b75cf4f25d74da1a850a40b4dae8
Anzelika Häberli <anzelika@joineer.ch>
2020-11-04 19:38:33 +0100

    setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1 class="text-center">HOME</h1>
+    <h1 class="text-center">JEST testing playground   <v-img class="d-inline-block" src="https://jestjs.io/img/jest.png" contain width="40"></v-img></h1>
+  
 
     <v-card class="mx-auto mt-2" width="600">
       <HelloWorld :title="title" :count="count" @increment="count++" @reset="count=0"/>
