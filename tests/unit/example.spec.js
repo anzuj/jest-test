c2125f14561ee4119beabc888df41d7971d940b2
Anzelika Häberli <anzelika@joineer.ch>
2020-11-05 09:03:10 +0100

    cleanup

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -23,13 +23,13 @@ describe("HelloWorld.vue", () => {
   };
 
   // TEST 1 ----------------------------------------------------
-    it("Mounts properly", () => {
+  it("Mounts properly", () => {
     const wrapper = mountFunction();
     expect(wrapper.findComponent(HelloWorld).exists()).toBe(true);
   });
 
   // TEST 2 ----------------------------------------------------
-    it("Title prop displays correctly", () => {
+  it("Title prop displays correctly", () => {
     const wrapper = mountFunction({
       propsData: { title: "Foobar" },
     });
@@ -37,7 +37,7 @@ describe("HelloWorld.vue", () => {
   });
 
   // TEST 3 ----------------------------------------------------
-  it("Emit custom event", async() => {
+  it("Emit custom event", async () => {
     const wrapper = mountFunction({
       propsData: { count: 0 },
     });
@@ -46,13 +46,13 @@ describe("HelloWorld.vue", () => {
     expect(button.exists()).toBe(true);
 
     const spy = spyOn(wrapper.vm, "increment");
-   await button.trigger("click");
+    await button.trigger("click");
     // expect(wrapper.emitted().increment).toBeTruthy() //why does not work?
     expect(wrapper.vm.increment).toBeCalled(); //why this works?
   });
 
-  // TEST 4 ----------------------------------------------------
- /*  it("Update DOM count", async () => {
+  // TEST 4 FAILS -------------------------------------------------
+  it("Update count in DOM", async () => {
     const wrapper = mountFunction({
       propsData: { count: 0 },
     });
@@ -60,11 +60,11 @@ describe("HelloWorld.vue", () => {
     await wrapper.vm.$emit("increment");
     // await Vue.nextTick()
     expect(wrapper.find("#nr").text()).toBe("1"); //after emit
-  }); */
+  });
 });
 
 // TEST 3 works, but unsure if specific enough to test button click
-/*    it('Emit custom event', () => {
+/*  it('Emit custom event', () => {
     const wrapper = mountFunction()
     
      wrapper.vm.$emit('increment');
